91ea1d65a9fbb8606155f6e3e1a811facf2d5898
Adurota01 <adurotapeter@gmail.com>
2022-09-24 01:21:32 +0100

    my commit

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
-"""
-import sys
-import urllib.request
+"""Takes URL, sends request, displays value X-Request-Id."""
+
+
+from sys import argv
+from urllib import request
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    with request.urlopen(argv[1]) as response:
+        att_header = response.info()["X-Request-Id"]
+        print(att_header)
